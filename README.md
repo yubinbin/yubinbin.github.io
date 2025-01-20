31b19f0a0606f8a91f64f2452cb14a37ce305923
yubinbin <940828782@qq.com>
2025-01-20 16:15:57 +0800

    --

--- README.md
+++
@@ -1,1 +0,0 @@
-# yubinbin.github.io
--- dist/getAssetsImages.js
+++
@@ -1,7 +0,0 @@
-// 动态获取图片
-
-const getAssetsImages = (name) => {
-    return new URL(`/src/assets/images/${name}`, import.meta.url).href
-}
-
-export default getAssetsImages;
